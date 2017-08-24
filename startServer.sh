b6a8c4d35a85e74a9fe1c0c5a5a6dda2f3c59c67
Sushma Ravichandran <sushma@sushmas-mbp.watson.ibm.com>
2017-08-24 10:42:25 -0400

    Dockerized the server

---
+++ startServer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "${ISTIO_ANALYTICS_SERVER_PORT}" ]; then
+  ISTIO_ANALYTICS_SERVER_PORT="5555"
+else
+  ISTIO_ANALYTICS_SERVER_PORT="${ISTIO_ANALYTICS_SERVER_PORT}"
+fi
+
+if [[ -z "${ISTIO_ANALYTICS_DEBUG}" ]]; then
+  ISTIO_ANALYTICS_DEBUG="false"
+else
+  ISTIO_ANALYTICS_DEBUG="${ISTIO_ANALYTICS_DEBUG}"
+fi
+
+export PYTHONPATH=/istio_analytics
+python3 /istio_analytics/istio_analytics_restapi/app.py
