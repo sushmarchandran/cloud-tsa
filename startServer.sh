410b203e03df984bbf51d865c79f3ffe0823d3ab
Sushma Ravichandran <sushma@sushmas-mbp.watson.ibm.com>
2017-09-08 14:46:16 -0400

    Added build docker script

--- startServer.sh
+++ startServer.sh
@@ -2,11 +2,14 @@
 
 #CREATING PYTHON VIRTUAL ENVIRONMENT
 apt-get update && apt-get install -y python3-pip
+adduser user
+apt-get install python3.6-venv
 python3.6 -m venv /python3.6-venv
-sudo source /python3.6-venv/bin/activate
+/bin/bash -c "source /python3.6-venv/bin/activate"
+
 
 #Running the requirements.txt file
-/python3.6-venv/bin/pip3.6 install -r requirements.txt
+python3.6-venv/bin/pip3.6 install -r istio_analytics/requirements.txt
 
 
 if [ -z "${ISTIO_ANALYTICS_SERVER_PORT}" ]; then
@@ -15,7 +18,7 @@ else
   ISTIO_ANALYTICS_SERVER_PORT="${ISTIO_ANALYTICS_SERVER_PORT}"
 fi
 
-if [[ -z "${ISTIO_ANALYTICS_DEBUG}" ]]; then
+if [ -z "${ISTIO_ANALYTICS_DEBUG}" ]; then
   ISTIO_ANALYTICS_DEBUG="false"
 else
   ISTIO_ANALYTICS_DEBUG="${ISTIO_ANALYTICS_DEBUG}"
@@ -23,4 +26,4 @@ fi
 
 export PYTHONPATH=/istio_analytics
 
-/python3.6-venv/bin/python /istio_analytics/istio_analytics_restapi/app.py
+python3.6-venv/bin/python /istio_analytics/istio_analytics_restapi/app.py
