17728696e62ce5d3068502e7907d8bd604dcf3cb
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2019-01-30 11:03:11 -0500

    removed duplicate buildandpush.py

--- iter8/cloudtsa/buildandpush.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import argparse
-
-parser = argparse.ArgumentParser(description='Dockerize CloudTSA')
-
-subprocess.call([f"docker-compose down --rmi all"], executable = "/bin/bash", shell = True)
-subprocess.call([f"docker-compose build"], executable = "/bin/bash", shell = True)
-# This assumes you have already logged in to docker from the shell where you're working...
-subprocess.call([f"docker-compose push"], executable = "/bin/bash", shell = True)
